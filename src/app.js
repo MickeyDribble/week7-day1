16dfd3fcd12dfe76ba3bac136e7a067afbe4ac0f
Michael <michaelf6988@gmail.com>
2022-04-26 10:25:12 +0100

    multiple

--- src/app.js
+++ src/app.js
@@ -6,13 +6,17 @@ const { Movie, movieArr } = require("./utils")
 
 const app = (yargsObj) => {
     if(yargsObj.add) {
-    const movie = new Movie(yargsObj.title, yargsObj.actor)
-    movie.add();
+    const movie1 = new Movie(yargsObj.title1,yargsObj.actor1,yargsObj.genre1, yargsObj.year1)
+    const movie2 = new Movie(yargsObj.title2,yargsObj.actor2,yargsObj.genre2, yargsObj.year2)
+    movie1.add();
+    movie2.add();
     console.log(movieArr);
 
     } else {
         console.log("Incorrect Command")
     }
+        
+    
 };
 
 app(yargs.argv);
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,21 +1,24 @@
 //Empty Array to store Data
 const movieArr = [];
-
 //Create Movie class for title and actor object
 //'this' refers to the object it belongs to
 class Movie {
-    constructor(title, actor = "Not specified") {
+    constructor(title, actor, genre, year = "Not specified") {
         this.title = title;
         this.actor = actor;
+        this.genre = genre;
+        this.year = year;
+        
     }
     //small function to add cli input to movieArr
     add() {
         movieArr.push(this)
     }
-    
+   
 };
+
 //export objects to use elsewhere in app
 module.exports = {
     movieArr,
-    Movie,  
+    Movie,
 };
